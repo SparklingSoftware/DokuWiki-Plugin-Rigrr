06dbc57f6079f884a74191a00519782832d81343
Stephan Dekker <Stephan@SparklingSoftware.com.au>
2012-12-03 21:10:21 +1100

    Added soo much functionality, its not funny...

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
 
-
 jQuery(window).load(function () {
-    //$(window).load(function() {
-    //jQuery(document).ready(function() {
-
-    // showPanel(jQuery.fn.jquery);
-    // setTimeout(function(){runRigrr() },500);
     runRigrr();
 });
-
-function showPanel(fieldName) {
-    var fieldNameElement = document.getElementById("theCanvas");
-    while (fieldNameElement.childNodes.length >= 1) {
-        fieldNameElement.removeChild(fieldNameElement.firstChild);
-    }
-    fieldNameElement.appendChild(fieldNameElement.ownerDocument.createTextNode(fieldName));
-}
 
 function runRigrr() {
     var BPMN = document.getElementById("rigrr_bpmn").value;
     try {
-        var lib = new com.rapilabs.Rigrr('theCanvas');
+        var lib = new com.rapilabs.Rigrr('rigrr_canvas');
         lib.drawDiagram("rigrr_canvas", BPMN);
     }
     catch (err) {
